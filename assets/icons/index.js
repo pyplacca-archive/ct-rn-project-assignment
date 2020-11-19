00d7d4af8946baf11442c628d21e723d2a7175f6
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-19 11:02:59 +0000

    manage export of icons

---
+++ assets/icons/index.js
@@ -0,0 +1,17 @@
+import cart from './cart.png';
+import heart from './heart.png';
+import redHeart from './red_heart.png';
+import redHeartOut from './red_heart_out.png';
+import left from './left.png';
+import star from './star.png';
+import starred from './starred.png';
+
+export {
+	cart,
+	heart,
+	left,
+	star,
+	starred,
+	redHeart,
+	redHeartOut,
+}
