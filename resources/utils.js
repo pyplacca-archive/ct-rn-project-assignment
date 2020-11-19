f6c964afa07d63d551d3a2f577b8badcee514008
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-19 11:11:03 +0000

    Create utils.js

---
+++ resources/utils.js
@@ -0,0 +1,23 @@
+export const styles = {
+};
+
+export const variables = {
+	colors: {
+		primary: '#757def',
+		primaryDark: '#4f4fec',
+		secondary: '#ffb400',
+		black: '#262626',
+		grey: '#bbbbbb',
+		greyLight: '#cdcdcd',
+		white: '#fff',
+		sea: '#f1f1f8',
+	},
+
+	dpi: {
+		xs: 5,
+		s: 15,
+		m: 30,
+		l: 70,
+		xl: 150,
+	}
+};
