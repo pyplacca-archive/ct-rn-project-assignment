4700cdecdc147facaec1c432a8fe90cfa2a3d7bf
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-19 19:13:31 +0000

    Update index.js

--- resources/components/index.js
+++ resources/components/index.js
@@ -1,10 +1,11 @@
-import Button from './Button';
-import Icon from './Icon';
 import SearchBar from './SearchBar';
 import Rating from './Rating';
+import Bar from './Bar';
+import ActionsBar from './ActionsBar';
 
 export {
-	Button,
-	Icon,
 	Rating,
+	SearchBar,
+	Bar,
+	ActionsBar,
 }
