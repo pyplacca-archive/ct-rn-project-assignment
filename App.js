50e4e623449c709f8eb8125736ff1042c7a3f717
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-19 11:18:49 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import ProductShowcase from './resources/screens/Showcase';
 
+// const {image, name, description, material, price, review, sizes} = props;
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ProductShowcase
+        name='Red Woman Jacket'
+        description='It is a long established fact that a reader will be distracted by the readable content of a page'
+        material='91% polyester, 9% elastane'
+        review={3}
+        sizes={['xs', 'S', 'M', 'L', 'XL']}
+        price={110}
+      />
     </View>
   );
 }
@@ -14,8 +22,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
