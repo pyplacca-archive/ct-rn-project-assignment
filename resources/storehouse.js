626712c87b78986f9fed15559ff9519fdf26af7f
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-19 19:17:18 +0000

    Create storehouse.js

---
+++ resources/storehouse.js
@@ -0,0 +1,10 @@
+const jackets = [
+	{name: 'Bike Jacket Softshell Warm', rating: 4, price: 99, },
+	{name: 'Pink Hoody Merino', rating: 3, price: 80,},
+	{name: 'Hooded Jacket Classic', rating: 5, price: 140},
+	{name: 'Red Woman Jacket', rating: 4, price: 115},
+];
+
+export default {
+	jackets
+}
