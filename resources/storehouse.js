8fd07a92f0705d1574c68445338694b1ad529ffe
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-20 15:29:11 +0000

    Update storehouse.js

--- resources/storehouse.js
+++ resources/storehouse.js
@@ -11,6 +11,7 @@ const jackets = [
 		price: 99,
 		image: black,
 		id: 'e2979a2e-eb53-4a19-80f1-7cc7aa07bee7',
+		sizes: ['S', 'M', 'XL'],
 	},
 	{
 		name: 'Brown Hood-ish Merino',
@@ -18,13 +19,15 @@ const jackets = [
 		price: 80,
 		image: turtleBrown,
 		id: '50f9c4ec-8e44-49d2-8fd1-d8750c2f34ff',
+		sizes: ['XS', 'M'],
 	},
 	{
+		id: '64721447-4968-4239-bf18-3773a17bfaca',
 		name: 'Gold Accent Classic Jacket',
 		rating: 5,
 		price: 140,
 		image: bng,
-		id: '64721447-4968-4239-bf18-3773a17bfaca',
+		sizes: ['S', 'M', 'L'],
 	},
 	{
 		name: 'Red Woman Jacket',
@@ -32,6 +35,9 @@ const jackets = [
 		price: 115,
 		image: red,
 		id:'f3caaa26-ba91-44b5-a9e2-a50544543daa',
+		material: '91% leather, 9% polyester',
+		description: 'It is a long established fact that a reader will be distracted by the readable content of a page',
+		sizes: ['XS', 'S', 'M', 'L', 'XL'],
 	},
 ];
 
