54e0a8b39f665c568d3bc392b6d0859ec94f8612
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-20 13:18:06 +0000

    include product images

--- resources/storehouse.js
+++ resources/storehouse.js
@@ -1,10 +1,45 @@
+import black from '../assets/images/products/jackets/black.png'
+import bng from '../assets/images/products/jackets/black-n-gold.png'
+import red from '../assets/images/products/jackets/red.png'
+import turtleBrown from '../assets/images/products/jackets/turtle-brown.png'
+
+
 const jackets = [
-	{name: 'Bike Jacket Softshell Warm', rating: 4, price: 99, },
-	{name: 'Pink Hoody Merino', rating: 3, price: 80,},
-	{name: 'Hooded Jacket Classic', rating: 5, price: 140},
-	{name: 'Red Woman Jacket', rating: 4, price: 115},
+	{
+		name: 'Bike Jacket Softshell Warm',
+		rating: 4,
+		price: 99,
+		image: black,
+		id: 'e2979a2e-eb53-4a19-80f1-7cc7aa07bee7',
+	},
+	{
+		name: 'Brown Hood-ish Merino',
+		rating: 3,
+		price: 80,
+		image: turtleBrown,
+		id: '50f9c4ec-8e44-49d2-8fd1-d8750c2f34ff',
+	},
+	{
+		name: 'Gold Accent Classic Jacket',
+		rating: 5,
+		price: 140,
+		image: bng,
+		id: '64721447-4968-4239-bf18-3773a17bfaca',
+	},
+	{
+		name: 'Red Woman Jacket',
+		rating: 4,
+		price: 115,
+		image: red,
+		id:'f3caaa26-ba91-44b5-a9e2-a50544543daa',
+	},
 ];
 
+const jacketPhotos = {
+	black, red, bng, turtleBrown
+}
+
 export default {
-	jackets
+	jackets,
+	jacketPhotos
 }
